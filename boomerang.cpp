0a422de5184cd7d295d644765fd51eab42ea8ec8
Kyle Guinn <elyk03@gmail.com>
2014-04-26 11:23:39 -0500

    Remove a cygwin workaround

--- boomerang.cpp
+++ boomerang.cpp
@@ -10,12 +10,7 @@
 #include "config.h"
 #endif
 
-#if __CYGWIN__
-#define USE_XML 0			// Cygwin has a weird problem that causes libBinaryFile.dll not to load if the expat library
-							// is used. Note that other Windows versions require expat.
-#else						// For all platforms other than Cygwin:
 #define USE_XML 0			// Set to 0 to not use the expat library for XML loading and saving
-#endif
 
 #include <iostream>
 #include <cstring>
