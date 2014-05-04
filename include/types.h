c67f233514220dd92f66d0c1ccfe39e90481139c
Kyle Guinn <elyk03@gmail.com>
2014-05-04 02:16:30 -0500

    Define machine types using stdint.h

--- include/types.h
+++ include/types.h
@@ -4,18 +4,20 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdint.h>
+
 // Machine types
-typedef unsigned char		Byte;		/* 8 bits */
-typedef unsigned short		SWord;		/* 16 bits */
-typedef unsigned int		DWord;		/* 32 bits */
-typedef unsigned int		dword;		/* 32 bits */
-typedef unsigned int		Word;		/* 32 bits */
-typedef unsigned int		ADDRESS;	/* 32-bit unsigned */
+typedef uint8_t  Byte;
+typedef uint16_t SWord;
+typedef uint32_t DWord;
+typedef uint64_t QWord;
+
+typedef DWord dword;
+
+typedef unsigned int ADDRESS;       /* 32-bit unsigned */
 
 
-#define STD_SIZE	32					// Standard size
-#define NO_ADDRESS ((ADDRESS)-1)		// For invalid ADDRESSes
-
-typedef long unsigned long QWord;		// 64 bits
+#define STD_SIZE    32              // Standard size
+#define NO_ADDRESS ((ADDRESS)-1)    // For invalid ADDRESSes
 
 #endif
